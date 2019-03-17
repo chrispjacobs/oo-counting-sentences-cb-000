634f160d9a7a89fbb779226eb5dce1343b499d3e
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-17 19:38:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split("?", ".", "!")
+    final_array = []
+    new_array.each do |item|
+      if item != ""
+        final_array << item
+      end
+    end
+    final_array
   end
 end
+
+puts count_sentences("What about this sentence?? Huh!?")
